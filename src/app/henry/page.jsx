d08a6bb3f8669658754cf3473535e0d802ef3caf
Julie Vogh <juvo0001@stud.kea.dk>
2024-04-11 12:04:05 +0200

    css

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,7 +1,8 @@
+import Image from "next/image";
+
 async function getHenry() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   return await res.json();
-  
 }
 
 export async function generateMetadata() {
@@ -19,10 +20,26 @@ export default async function HenryPage() {
   const { name, favouriteColor, age } = data;
 
   return (
-    <main>
-      <h1>{name}</h1>
-      
-      <p></p>
+    <main className="md:flex max-w-7xl mx-auto">
+      <Image
+        src="https://images.unsplash.com/photo-1517849845537-4d257902454a"
+        alt="A cute dog"
+        width={3024}
+        height={4032}
+        priority={true} // disables lazy load
+        className="w-full md:w-1/2 xl:w-[600px]"
+        sizes="(max-width: 768px) 100vw,
+         (max-width: 1280px) 50vw,
+         600px"
+      />
+      <div>
+        <h1 className="text-3xl">My name is {name}</h1>
+        <p>
+          My favorite color is <span className="text-yellow-400">{favouriteColor}</span>
+        </p>
+
+        <p>I am {age} years old. </p>
+      </div>
     </main>
   );
 }
